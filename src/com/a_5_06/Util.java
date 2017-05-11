05449a10595322d079ba6d8ccddb2ad88c2c73d8
hongwei666 <ghwmail@163.com>
2017-05-12 01:02:43 +0800

    add Util

--- src/com/a_5_06/Util.java
+++ src/com/a_5_06/Util.java
@@ -5,9 +5,19 @@ package com.a_5_06;
  */
 public class Util {
     //交换数组中俩个元素的值
-    public static void swap(int[] a, int index1, int index2) {
+    static void swap(int[] a, int index1, int index2) {
         int temp = a[index1];
         a[index1] = a[index2];
         a[index2] = temp;
+    }
+
+    static void compare(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    swap(arr, i, j);
+                }
+            }
+        }
     }
 }
