ad29aab2bb75eba1f571b052774259851d749af8
hongwei666 <ghwmail@163.com>
2017-05-14 11:18:47 +0800

    add suiji Methed2

--- src/com/a_5_13/suiji.java
+++ src/com/a_5_13/suiji.java
@@ -1,6 +1,5 @@
 package com.a_5_13;
 
-import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -9,20 +8,39 @@ import java.util.Scanner;
  */
 public class suiji {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        int zs = scanner.nextInt();
-        Random random = new Random();
-        char[] sz = new char[zs];
-        for (int i = 0; i < zs; i++) {
-            int b = random.nextInt(3);
-            if (b == 0) {
-                sz[i] = (char) (random.nextInt(10) + 48);
-            } else if (b == 1) {
-                sz[i] = (char) (random.nextInt(26) + 65);
-            } else if (b == 2) {
-                sz[i] = (char) (random.nextInt(26) + 97);
+        System.out.println("请输入一个整数");
+        int shuru = scanner.nextInt();
+        char[] s = new char[shuru];
+        for (int i = 0; i < shuru; i++) {
+            int a = (int) (Math.random() * 3);
+            if (a == 0) {
+                s[i] = (char) (Math.random() * 10 + 48);
+            } else if (a == 1) {
+                s[i] = (char) (Math.random() * 26 + 65);
+            } else {
+                s[i] = (char) (Math.random() * 26 + 97);
             }
         }
-        System.out.println(sz);
+        System.out.println(s);
+/**
+ *方法二用Random
+ */
+//        Scanner scanner = new Scanner(System.in);
+//        int zs = scanner.nextInt();
+//        Random random = new Random();
+//        char[] sz = new char[zs];
+//        for (int i = 0; i < zs; i++) {
+//            int b = random.nextInt(3);
+//            if (b == 0) {
+//                sz[i] = (char) (random.nextInt(10) + 48);
+//            } else if (b == 1) {
+//                sz[i] = (char) (random.nextInt(26) + 65);
+//            } else if (b == 2) {
+//                sz[i] = (char) (random.nextInt(26) + 97);
+//            }
+//        }
+//        System.out.println(sz);
     }
 }
